497bc77ff68e3b3e1664c6001c4d76d984143f69
Mo Ki <javatimedev@gmail.com>
2024-12-05 12:01:00 -0500

    countFactor-improved

--- codility/CountFactors.java
+++ codility/CountFactors.java
@@ -4,16 +4,20 @@ public class CountFactors {
     public static int solution(int num) {
 
         if (num == 1) {
-            return 1; // 1 has exactly one factor: itself
+            return 1;
         }
         if (num == 2 || num == 3) {
-            return 2; // 2 has two factors: 1 and 2
+            return 2;
         }
 
-        int count = 1; // Start with 1 for the number itself
+        int count = 1;
         int exponent = 0;
         int temp = num;
-        for (int i=2; i*i<=temp; i++){// 2= first prime number
+        for (int i=2; i<=temp/i; i++){
+            // 2= first prime number, loop through all numbers from 2 to the square root of num
+            // We only need to check up to the square root of num, since factors come in pairs
+            // If i is a factor of temp, we will count how many times it divides temp
+            // and multiply the count by (exponent + 1) to account for all factors
             // check up to the square root of num, since factors come in pairs
             if (temp % i == 0) {
                 exponent = 0;
@@ -27,9 +31,8 @@ public class CountFactors {
             }
         }
         if (temp > 1) {
-            // If temp is still greater than 1, it means it's a prime factor itself
-            count *= 2; // It contributes two factors: 1 and itself
-            
+            // If temp is still greater than 1, it means it is a prime factor itself
+            count *= 2;
         }
 
         return count;
--- codility/Main.java
+++ codility/Main.java
@@ -2,37 +2,37 @@ public class Main {
     // This is a simple Java program that prints "Hello world!" to the console.
     public static void main(String[] args) {
 
-        frogJump frogJump = new frogJump();
-        int X = 10;
-        int Y = 85;
-        int D = 30;
-        System.out.println("Number of jumps: " + frogJump.solution(X, Y, D));
+        // frogJump frogJump = new frogJump();
+        // int X = 10;
+        // int Y = 85;
+        // int D = 30;
+        // System.out.println("Number of jumps: " + frogJump.solution(X, Y, D));
 
-        Distinct distinct = new Distinct();
-        int[] Arr = {2, 1, 1, 2, 3, 1};
-        System.out.println("Number of distinct elements: " + distinct.solution(Arr));
+        // Distinct distinct = new Distinct();
+        // int[] Arr = {2, 1, 1, 2, 3, 1};
+        // System.out.println("Number of distinct elements: " + distinct.solution(Arr));
 
-        Nested nested = new Nested();
-        String S = "{[()]}";
-        if(nested.nested(S) == 1) {
-            System.out.println(S + " is properly nested");
-        } else {
-            System.out.println(S + " is not nested");
-        }
+        // Nested nested = new Nested();
+        // String S = "{[()]}";
+        // if(nested.nested(S) == 1) {
+        //     System.out.println(S + " is properly nested");
+        // } else {
+        //     System.out.println(S + " is not nested");
+        // }
 
-        Dominator dom = new Dominator();
-        int[] A = {3, 4, 3, 2, 3, -1, 3, 3};
-        int result = dom.dominator(A);
-        if(result != -1) {
-            System.out.println("Dominator is: " + A[result] + " at index: " + result);
-        } else {
-            System.out.println("No dominator found");
-        }
+        // Dominator dom = new Dominator();
+        // int[] A = {3, 4, 3, 2, 3, -1, 3, 3};
+        // int result = dom.dominator(A);
+        // if(result != -1) {
+        //     System.out.println("Dominator is: " + A[result] + " at index: " + result);
+        // } else {
+        //     System.out.println("No dominator found");
+        // }
 
-        MaxProfit maxProfit = new MaxProfit();
-        int[] dailyPrices = {23199, 21010, 21123, 21366, 21013, 21367};
-        int profit = maxProfit.solution(dailyPrices);
-        System.out.println("Maximum profit: $" + profit);
+        // MaxProfit maxProfit = new MaxProfit();
+        // int[] dailyPrices = {23199, 21010, 21123, 21366, 21013, 21367};
+        // int profit = maxProfit.solution(dailyPrices);
+        // System.out.println("Maximum profit: $" + profit);
 
         Euclidean euclidean = new Euclidean();
         int numOfChoco = 10;
@@ -43,6 +43,10 @@ public class Main {
         CountFactors countFactors = new CountFactors();
         int num = 24;
         int countedFac = countFactors.solution(num);
+        System.out.println("Number of factors of " + num + ": " + countedFac);
+
+        num = 2000111229;
+        countedFac = countFactors.solution(num);
         System.out.println("Number of factors of " + num + ": " + countedFac);
     }
     

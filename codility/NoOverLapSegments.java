030e6d42eda3ecdf14a8554848fdad7b4e9200a8
Mo Ki <javatimedev@gmail.com>
2024-12-08 12:01:00 -0500

    moveOn

--- codility/NoOverLapSegments.java
+++ codility/NoOverLapSegments.java
@@ -1,6 +1,6 @@
 public class NoOverLapSegments {
     public int solution(int[] A, int[] B) {
-        if (A.length == 0 || B.length == 0) {
+        if (A.length == 0) {
             return 0;
         }
 
@@ -8,10 +8,10 @@ public class NoOverLapSegments {
         int lastEnd = -1;
 
         for (int i = 0; i < A.length; i++) {
-            if (A[i] > lastEnd) {
+            if (A[i] > lastEnd) { // If the start of the current segment is greater than the end of the last non-overlapping segment
                 nonOverlapCount++;
-                lastEnd = B[i];
-            } else if (B[i] > lastEnd) {
+                lastEnd = B[i]; // Update the end of the last non-overlapping segment
+            } else if (B[i] > lastEnd) { 
                 lastEnd = B[i];
             }
         }

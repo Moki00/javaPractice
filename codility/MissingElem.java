6b5e1ab62f8ec3acfb6a0607bd1962cbe97050fe
Mo Ki <javatimedev@gmail.com>
2024-12-11 12:01:00 -0500

    findMissingElement2

--- codility/MissingElem.java
+++ codility/MissingElem.java
@@ -6,19 +6,16 @@ public class MissingElem{
         }
         
         // Calculate the expected sum of the first N natural numbers
-        long n = A.length + 1; // within the range [0..100,000], +1 for the missing one
+        long n = A.length + 1L; // within the range [0..100,000], +1 for the missing one
         long expectedSum = n * (n + 1) / 2;
         
         // Calculate the actual sum of the elements in the array
         long actualSum = 0;
-        for (int num : A) {
-            actualSum += num;
+        for (int element : A) {
+            actualSum += element;
         }
-
-        int missingElement = 0;
-        missingElement = (int) (expectedSum - actualSum);
         
         // The missing element is the difference between expected and actual sums
-        return missingElement;
+        return (int) (expectedSum - actualSum);
     }
 }

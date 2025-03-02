275524905d1b63931595e1946f8835b503fa529f
Mo Ki <javatimedev@gmail.com>
2025-03-02 12:09:00 -0500

    no overlap example in Main

--- codility/Main.java
+++ codility/Main.java
@@ -4,13 +4,12 @@ public class Main {
     // This is a simple Java program that prints "Hello world!" to the console.
     public static void main(String[] args) {
         
-        // Example usage of DistinctSlices
-        DistinctSlices distinctSlices = new DistinctSlices();
-        int M = 6; // Maximum value in array A
-        int[] B = {3, 4, 5, 5, 2};
-        int distinctCount = distinctSlices.solution(M, B);
-        System.out.println("Number of distinct slices in array " + Arrays.toString(B) + ": " + distinctCount);
-
+        // No Overlapping Segments
+        NoOverLapSegments noOverlapping = new NoOverLapSegments();
+        int[] A = {1, 3, 7, 9, 9};
+        int[] B = {5, 6, 8, 9, 10};
+        int result = noOverlapping.solution(A, B);
+        System.out.println("Number of non-overlapping segments: " + result);
 
     }
     

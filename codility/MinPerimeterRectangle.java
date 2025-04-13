628cc1f32523b637784c200db3b6e7da133dd8d0
Mo Ki <javatimedev@gmail.com>
2025-04-13 12:09:00 -0400

    Min Perimeter of rectangle is better with Math

--- codility/MinPerimeterRectangle.java
+++ codility/MinPerimeterRectangle.java
@@ -1,25 +1,20 @@
 public class MinPerimeterRectangle {
 
     public int solution(int N) {
-        // Initialize the minimum perimeter to a large value
-        int minPerimeter = Integer.MAX_VALUE;
-        // Iterate through all possible factors of N
+
+        int perimeter = Integer.MAX_VALUE;
+
         for (int i = 1; i <= Math.sqrt(N); i++) {
             if (N % i == 0) {
                 // i is a factor, so N / i is the corresponding factor
                 int width = i;
                 int height = N / i;
-                // Calculate the perimeter of the rectangle
-                int perimeter = 2 * (width + height);
-                // Update the minimum perimeter if the current one is smaller
-                if (perimeter < minPerimeter) {
-                    minPerimeter = perimeter;
-                }
+                perimeter = Math.min(perimeter, 2 * (width + height));
             }
         }
 
 
-        return minPerimeter;
+        return perimeter;
     }
     
 }
